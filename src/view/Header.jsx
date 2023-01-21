f002a75360e27481f92fee35609452681ffb5c3b
Silvia.A.A <silvia.alvarez.allende@se.experis.com>
2023-01-21 19:15:38 +0100

    Component Tree Bootstrap Header

---
+++ src/view/Header.jsx
@@ -0,0 +1,40 @@
+// import '../App.css';
+import Logo from '../asset/Logo.png'
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+// const Header = () => {
+//     return (
+//     <div>
+//     <header>
+//         <img src={Logo} alt="A happy wavy robot" />
+//         <h1>Lost in Translation</h1>
+//     </header>
+//     </div>)
+// } 
+
+
+const Header = () => {
+    return (
+    <header>
+        <Navbar>
+        <Container>
+          <Navbar.Brand>
+            <img
+              alt="A happy wavy robot"
+              src={Logo}
+              width="30"
+              height="30"
+              className="d-inline-block align-right"
+            />{' '}
+            <h1>Lost in Translation</h1>
+          </Navbar.Brand>
+        </Container>
+      </Navbar>
+    </header>
+    )
+} 
+
+
+
+export default Header;
