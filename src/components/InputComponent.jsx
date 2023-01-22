cd39fc4307ff64e2c18ba4605c10efd952c2c8c1
Lazkin Youssuf <lazkin.youssuf96@yahoo.com>
2023-01-22 20:44:29 +0100

    InputComponent-Icons

--- src/components/InputComponent.jsx
+++ src/components/InputComponent.jsx
@@ -1,10 +1,21 @@
 import { Form, InputGroup, Button } from 'react-bootstrap';
+import keyboard from '../../node_modules/bootstrap-icons/icons/keyboard-fill.svg'
+import arrow from '../../node_modules/bootstrap-icons/icons/arrow-right-circle.svg'
+import {useForm} from 'react-hook-form'
+
+
 
 const InputComponent = () => {
+  const handleSubmit = (event) =>{ 
+    console.log("Succeed!");
+  }
+
   return (
     <>
     <style type="text/css">
         {`
+    
+
     .btn-flat {
       background-color: purple;
       color: white;
@@ -14,16 +25,17 @@ const InputComponent = () => {
     }
     `}
       </style>
-      <Form >
+      <Form style={{marginLeft : "30%", marginRight : "20%" , border: 'solid', padding:'40px' ,borderRadius: '2%' , marginTop:'14em', position:'absolute', zIndex:'10'
+          , backgroundColor: 'white'}} onSubmit={handleSubmit}>
         <InputGroup className="mb-3">
           <InputGroup.Text>
-          <i>Key</i>
+          <img src={keyboard} alt='keyBoard' width={'30px'} height={'30px'}/>
           </InputGroup.Text>
-          <Form.Control aria-label="Amount (to the nearest dollar)" className='btn-inputgroup-bg' />
+          <Form.Control aria-label="Amount (to the nearest dollar)" className='btn-inputgroup-bg' placeholder='What is your name?'  />
 
           <InputGroup.Text >
-            <Button variant="outline-success" className="rounded-circle btn-flat">
-             @
+            <Button variant="outline-success" className="rounded-circle btn-flat" type="submit" >
+            <img src={arrow} alt='arrow' width={'25px'} height={'30px'}/>
             </Button>
           </InputGroup.Text>
 
--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,41 +1,47 @@
 import Logo from '../asset/Logo.png'
 import LogoCloud from '../asset/Splash.svg'
+import InputComponent from './InputComponent'
 
-const LoginForm = () =>{
+
+const LoginForm = () => {
     return (
         <>
-       
-        <div className="div-header" style={{backgroundColor : ' #FFC75F' , borderBottom : 'solid' , borderColor : '#969696'}}>
-            <h1 style={{color : '#EFEFEF', paddingLeft: '10%', paddingTop: '12px' }}>Lost in Translation</h1>
-        </div>
-        <div className="container" style={{backgroundColor : ' #FFC75F' ,display: 'flex', height: '200px'}}>
-        <img
-              alt="Cloud Image"
-              src={LogoCloud}
-              width="120"
-              height="120"
-              style={{paddingLeft:'20%', paddingTop: '3%'}}
-              className="image1"
-            />
-        <img
-            alt="A happy wavy robot"
-            src={Logo}
-            width="100"
-            height="100"
-            style={{position :'absolute', paddingLeft:'21%', paddingTop: '5%'}}
-            className="image2"
-        
-        />
-            <div className='text' style={{display :'flex', flexDirection : 'column' , justifyContent: 'center',
-                paddingLeft: '6.5em' }}>
-                <h1 style={{color : '#EFEFEF' }}>Lost in Translation</h1>
-                <h3 style={{color : '#EFEFEF'}}>Get Started</h3>
-            </div>
-        </div>
-       
-        <form>
 
-        </form>
+            <div className="div-header" style={{ backgroundColor: ' #FFC75F', borderBottom: 'solid', borderColor: '#969696' }}>
+                <h1 style={{ color: '#EFEFEF', paddingLeft: '10%', paddingTop: '12px' }}>Lost in Translation</h1>
+            </div>
+            <div className="container" style={{ backgroundColor: ' #FFC75F', display: 'flex', height: '300px', maxWidth : 'inherit' , position:'absolute'}}>
+                <div style={{position: 'relative', top: '0', left: '0' , marginLeft : '10em' ,marginTop: '2.5em' }}>
+                    <img
+                        alt="Cloud Image"
+                        src={LogoCloud}
+                        width="120"
+                        height="120"
+                        style={{ left:'0' , position: 'relative', top : '0'}}
+                        className="image1"
+                    /> 
+                    <img
+                        alt="A happy wavy robot"
+                        src={Logo}
+                        width="100"
+                        height="100"
+                        style={{  left:'12px' , top: '12px' , position: 'absolute' }}
+                        className="image2"
+
+                    />
+                </div>
+                <div className='text' style={{
+                    display: 'flex', flexDirection: 'column', justifyContent: 'center',
+                    paddingLeft: '6.5em', paddingBottom: '5em'
+                }}>
+                    <h1 style={{ color: '#EFEFEF' }}>Lost in Translation</h1>
+                    <h3 style={{ color: '#EFEFEF' }}>Get Started</h3>
+                </div>
+            </div>
+
+            <form>
+                <InputComponent />
+            </form>
         </>
     )
 }
