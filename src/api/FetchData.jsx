604e08aca1cb4f53d02d7f85bb9ce7107627345c
Silvia.A.A <silvia.alvarez.allende@se.experis.com>
2023-01-24 08:00:41 +0100

    fetch in api directory

---
+++ src/api/FetchData.jsx
@@ -0,0 +1,31 @@
+import { useEffect } from "react"
+
+const apiURL = 'https://grandiose-persistent-bread.glitch.me';
+const apiKey = ''
+
+// Translation Users GET /translations?username=<query>
+const FetchData = ({username}) => {
+
+    useEffect(() => {
+        const fetchTranslation = async () => {
+            try {
+                const response = await fetch(`${apiURL}/translations?username=${username}`);
+                if (response.ok) {
+                   throw new Error("no User")
+                }
+                const data = await response.json();
+               
+               return [null, data]
+               
+            } catch (error) {
+                console.log(error)
+            }
+        };
+        
+        if (username) {
+            fetchTranslation()
+        } 
+    }, [username]);
+}
+
+export default FetchData;
